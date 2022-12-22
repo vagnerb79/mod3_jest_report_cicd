b5d2c0cf69677780413ac66062ba9c2f34d7f587
CarlosEduardo1984 <33332202+carloseduardo1984@users.noreply.github.com>
2022-12-22 23:26:44 +0000

    Update soma.test.js

--- tests/soma.test.js
+++ tests/soma.test.js
@@ -5,3 +5,10 @@ test('Adicione 1 + 2 e resulte em 3', () => {
     expect(soma(1, 2)).toBe(3);
 
 });
+
+test('Adicione 1 + 2 e não resulte em 5', () => {
+
+    expect(soma(1, 2)).not.toBe(5);
+
+});
+
